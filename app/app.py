5a84e3c4663a4bd0b3e4e829e03d5c0ca26d8c24
Vincenzo Heska <vincenzo.heska@gmail.com>
2025-05-21 16:24:32 -0400

    Cited sources for example photos

--- app/app.py
+++ app/app.py
@@ -12,7 +12,15 @@ iface = gr.Interface(
         ["examples/dog.jpg"],
         ["examples/bike.jpg"],
         ["examples/pizza.jpg"]
-    ]
+    ],
+    article="""
+   <p>Dog photo by <a href="https://unsplash.com/@mtsjrdl?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">mtsjrdl</a> on 
+   <a href="https://unsplash.com/photos/white-and-brown-long-coated-dog-5yAhL8ViUVg?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a></p>
+   <p>Bike photo by <a href="https://unsplash.com/@streetsh?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">streetsh</a> on 
+   <a href="https://unsplash.com/photos/black-and-orange-road-bike-parked-beside-white-wall-vZAk_n9Plfc?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a><p>
+   <p>Pizza photo by <a href="https://unsplash.com/@thenixcompany?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">The Nix Company</a> on 
+   <a href="https://unsplash.com/photos/sliced-pizza-on-white-ceramic-plate-evA8uQLsUaE?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a></p>
+   """
 )
 
 if __name__ == "__main__":
